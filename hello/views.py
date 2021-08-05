4e2abd3c38541b0e66b0002b136cce7131d4fd30
vijayakumarece08 <70196404+vijayakumarece08@users.noreply.github.com>
2021-08-05 03:28:25 -0700

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -6,7 +6,7 @@ import requests
 import nsepy
 from datetime import date
 import pandas as pd
-from py5paisa.strategy import *
+#from py5paisa.strategy import *
 
 # Create your views here.
 #def index(request):
@@ -44,13 +44,13 @@ def get_vix_value():
     
 
     # Assume yesterday as today
-    yesterday = date.today();
+    #yesterday = date.today();
     #while True:
         # Get yesterday date as we are going to run this before market hours
         #yesterday = yesterday;
 
         # Get VIX value of yesterday
-    df = pd.DataFrame(nsepy.get_history(symbol="INDIAVIX", start=date(2015,1,1), end=date(2015,1,10), index=True));
+   # df = pd.DataFrame(nsepy.get_history(symbol="INDIAVIX", start=date(2015,1,1), end=date(2015,1,10), index=True));
     #df;
     #print ("after");
         # IF no data found on yesterday, go back to previous day because it may be weekend
@@ -64,25 +64,25 @@ def get_vix_value():
 def index(request):
     print ("Inside Index  - Hello World");    
     
-    from py5paisa import FivePaisaClient
-    client = FivePaisaClient(email="lakshmifemina7@gmail.com", passwd="Vinayakk@123", dob="19920710")
-    client.login()
-    req_list_=[{"Exch":"N","ExchType":"D","Symbol":"NIFTY 29 JUL 2021 CE 15200.00","Expiry":"20210729","StrikePrice":"15200","OptionType":"CE"},
+   # from py5paisa import FivePaisaClient
+   # client = FivePaisaClient(email="lakshmifemina7@gmail.com", passwd="Vinayakk@123", dob="19920710")
+   # client.login()
+   # req_list_=[{"Exch":"N","ExchType":"D","Symbol":"NIFTY 29 JUL 2021 CE 15200.00","Expiry":"20210729","StrikePrice":"15200","OptionType":"CE"},
             {"Exch":"N","ExchType":"D","Symbol":"NIFTY 29 JUL 2021 PE 15200.00","Expiry":"20210729","StrikePrice":"15200","OptionType":"PE"}]
 
     #dict1 = client.fetch_market_feed(req_list_)
-    print(client.fetch_market_feed(req_list_))
+    #print(client.fetch_market_feed(req_list_))
     
-    dict1=Client.Request_Feed('mf','s',req_list)
-    client.Streming_data(dict1)
+    #dict1=Client.Request_Feed('mf','s',req_list)
+    #client.Streming_data(dict1)
     # Fetches holdings
-    print(client.holdings())
+    #print(client.holdings())
 
     # Fetches margin
-    client.margin()
+   # client.margin()
 
     # Fetches positions
-    client.positions()
+    #client.positions()
 
     # Fetches the order book of the client
     #print(client.order_book())
